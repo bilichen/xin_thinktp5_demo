a98e6f91bd6197e54b3418362fb47be3a5aa47c7
Administrator <1880161@qq.com>
2017-10-30 14:29:07 +0800

    11

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,8 +6,8 @@ class Index
     public function index()
     {
         \think\config::load(APP_PATH.'../config/abc/config.php');
-        return config('abc_name');
-//        dump(\think\config::get());
+//        return config('abc_name');
+        dump(\think\config::get());
     }
 
     public function add($n=0,$m=0){
